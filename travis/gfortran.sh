9961751373157ee5d89602b9870958203c05b8c8
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-26 10:54:19 +0100

    Remove the old Fortran-installation script

--- travis/gfortran.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-  export CGO_LDFLAGS="-L$(dirname $(find $(brew --prefix) -name libgfortran.dylib | grep -v i386))";
-fi
-
-gfortran --version
